4c0736eef694a7daddc5ea40ef647a504da8592f
gaiac <gaia.casalini@edu.cobianchi.it>
2023-02-14 11:15:21 +0100

    ciaociao

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Fede presto si incazza!");
+        System.out.println("feugeihuieha!");
     }
 }
