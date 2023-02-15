f6855c39c9fcab6af74aab2f1708d57d33e4e5b8
mirko <mirko.galbasini@edu.cobianchi.it>
2023-02-15 11:14:17 +0100

    prova grafica

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,12 @@
 package Main;
+
 import View.NostroFrame;
 
-public class Main  {
+public class Main {
 
     public static void main(String[] args) {
-            System.out.println("Hello world!");
-            System.out.println("feugeihuieha!");
-            NostroFrame nf = new NostroFrame();
+        System.out.println("Hello world!");
+        System.out.println("feugeihuieha!");
+        NostroFrame nf1 = new NostroFrame();
     }
 }
--- src/View/NostroFrame.java
+++ src/View/NostroFrame.java
@@ -1,11 +1,17 @@
 package View;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NostroFrame extends JFrame {
+    private JPanel JPanel1;
+    private JButton button1;
+
     public NostroFrame() {
-        super("Il Paroliere");
+        setTitle("il paroliere");
         setSize(500, 300);
+        setContentPane(JPanel1);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
