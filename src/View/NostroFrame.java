4679cc1af3e1f2886a7fc44ec0fd5aa9c7022c88
gaiac <gaia.casalini@edu.cobianchi.it>
2023-03-07 12:15:17 +0100

    vaffanculo galba

--- src/View/NostroFrame.java
+++ src/View/NostroFrame.java
@@ -1,16 +1,31 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NostroFrame extends JFrame {
     private JPanel JPanel1;
     private JButton button1;
+    private JButton button2;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
 
     public NostroFrame() {
-        setTitle("il paroliere");
-        setSize(500, 300);
+        setTitle("Il Paroliere");
+        setSize(600, 500);
         setContentPane(JPanel1);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
