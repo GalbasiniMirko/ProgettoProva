9f4428aeea18c93744d08489e1f861d8a24e582a
mirko <mirko.galbasini@edu.cobianchi.it>
2023-03-23 14:29:03 +0100

    sistemazione codice

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,16 +13,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //ImageIcon image = new ImageIcon("");
+        //ImageIcon image = new ImageIcon("");   //create image
 
         JLabel label = new JLabel();   //create a label
         label.setText("Bro, do you even code?");   //set text of label
-        //label.setIcon(image);
+        //label.setIcon(image);   //image of label
+        label.setHorizontalTextPosition(JLabel.CENTER);   //set text LEFT, CENTER, RIGHT of ImageIcon
+        label.setVerticalTextPosition(JLabel.TOP);   //set text TOP, CENTER, BOTTOM of ImageIcon
+        label.setForeground(new Color(0, 0, 0));   //set font color of text
+        label.setFont(new Font("MV Boli", Font.PLAIN, 20));   //set font of text
+        label.setIconTextGap(-25);   //set gap of text to image
+        label.setBackground(new Color(253,168,90));   //set background color
+        label.setOpaque(true);   //display background color
 
         NostroFrame frame = new NostroFrame();   //creates a frame
-        frame.add(label);   //
+        frame.add(label);   //add label to frame
 
-        List<String> stngFile = new ArrayList<String>();
+        /*List<String> stngFile = new ArrayList<String>();
         Scanner scnr = null;
         try {
             scnr = new Scanner(new FileReader(
@@ -40,6 +47,6 @@ public class Main {
             System.out.println(eachString);
         }
         //prova per vedere se funziona
-        System.out.println(array[20]);
+        System.out.println(array[20]);*/
     }
 }
